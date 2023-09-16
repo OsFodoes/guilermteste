f8a65d1b6d40f6f7708ca32d85f2acdc2696c1b2
unknown <guilhermenunessoares88765@gmail.com>
2023-09-15 22:34:52 -0300

    Ola fodoes

---
+++ fodoessss/fodoess.java
@@ -0,0 +1,16 @@
+package fodoessss;
+
+public class fodoess {
+	public static void main(String[] args) {
+		System.out.println("Alberto1");
+		System.out.println("Alberto");
+		System.out.println("Alberto");
+		System.out.println("Alberto");
+		System.out.println("Alberto");
+		System.out.println("Alberto");
+		System.out.println("Alberto");
+		System.out.println("Alberto");
+		System.out.println("Alberto");
+	}
+	//sysoutasasas
+}
